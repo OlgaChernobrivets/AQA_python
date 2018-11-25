e1512c312b170e1d01066ac6dbb95b618017661b
Olga Chernobrivets <gmco555@gmail.com>
2018-11-26 01:58:13 +0600

    for PR

--- lesson 2/planner.py
+++ lesson 2/planner.py
@@ -3,7 +3,7 @@ import time
 from selenium import webdriver
 from selenium.webdriver.common.by import By
 from selenium.webdriver.support.ui import WebDriverWait
-from selenium.webdriver.support import expected_conditions as EC, ui
+from selenium.webdriver.support import expected_conditions as EC
 from selenium.webdriver.support.select import Select
 from selenium.common.exceptions import NoSuchElementException
 from pages import Page
